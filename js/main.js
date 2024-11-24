895463301d3afad844991a20fc6ea1306723de60
menna-ali140 <mennagalal405@gmail.com>
2024-11-24 16:37:48 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementById("hi").style.textAlign="center";
+document.getElementById("paragraph").style.color="rgb(255,0,0)";
+function myfun(){
+    document.getElementById("fun").innerHTML=Date();
+}
